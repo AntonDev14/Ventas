839b9060798469959e43f3497743f434f99559b2
AntonDev14 <jairo_llaca@pegatroncorp.com>
2026-08-28 14:08:07 -0600

    Refactor update and delete methods for better querying

--- supabase.js
+++ supabase.js
@@ -15,8 +15,16 @@
                     };
                 },
                 insert: (values) => this._request('POST', table, values),
-                update: (values) => this._request('PATCH', table, values),
-                delete: () => this._request('DELETE', table, null)
+                update: (values) => {
+                    return {
+                        eq: (colName, colVal) => this._request('PATCH', `${table}?${colName}=eq.${colVal}`, values)
+                    };
+                },
+                delete: () => {
+                    return {
+                        eq: (colName, colVal) => this._request('DELETE', `${table}?${colName}=eq.${colVal}`, null)
+                    };
+                }
             };
         }
         _request(method, table, payload) {
@@ -32,7 +40,7 @@
                     'apikey': this.supabaseKey,
                     'Authorization': `Bearer ${this.supabaseKey}`,
                     'Content-Type': 'application/json',
-                    'Prefer': 'return=representation' // Obliga a Supabase a devolver el objeto JSON creado
+                    'Prefer': 'return=representation'
                 }
             };
             
@@ -41,11 +49,9 @@
             }
             
             return fetch(url, options).then(res => {
-                // Si la respuesta no tiene contenido (204) o está vacía pero es exitosa
                 if (res.status === 204) return { data: [], error: null };
                 
                 return res.text().then(text => {
-                    // Verificar si el texto recibido está vacío
                     const data = text ? JSON.parse(text) : [];
                     return { data: res.ok ? data : null, error: res.ok ? null : data };
                 });
